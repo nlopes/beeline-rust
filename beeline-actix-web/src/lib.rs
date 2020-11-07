03025ba4f90f178ea19d10d1f834d66a5a6b0ae5
Norberto Lopes <nlopes.ml@gmail.com>
2020-11-07 13:22:49 +0000

    Bump versions of all packages

--- beeline-actix-web/src/lib.rs
+++ beeline-actix-web/src/lib.rs
@@ -50,6 +50,7 @@ fn main() -> std::io::Result<()> {
 #![deny(missing_docs)]
 
 use std::marker::PhantomData;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::time::SystemTime;
 
@@ -61,8 +62,12 @@ use actix_web::{
     Error,
 };
 use beeline::{Client, SafeTrace, Sender};
-use futures::future::{ok, FutureResult};
-use futures::{Async, Future, Poll};
+use futures::{
+    future::{ok, Ready},
+    task::{Context, Poll},
+    Future,
+};
+use pin_project::{pin_project, pinned_drop};
 use serde_json::json;
 
 #[derive(Debug, Clone)]
@@ -125,7 +130,7 @@ where
     type Error = Error;
     type InitError = ();
     type Transform = BeelineService<S, T>;
-    type Future = FutureResult<Self::Transform, Self::InitError>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
         ok(BeelineService {
@@ -145,7 +150,6 @@ pub struct BeelineService<S, T: Sender + Clone> {
 impl<T, S, B> Service for BeelineService<S, T>
 where
     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
-    S::Future: 'static,
     B: MessageBody,
     T: Sender + Clone,
 {
@@ -154,8 +158,8 @@ where
     type Error = S::Error;
     type Future = BeelineServiceResponse<S, B, T>;
 
-    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
-        self.service.poll_ready()
+    fn poll_ready(&mut self, ct: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(ct)
     }
 
     fn call(&mut self, req: ServiceRequest) -> Self::Future {
@@ -169,12 +173,14 @@ where
 }
 
 #[doc(hidden)]
+#[pin_project::pin_project]
 pub struct BeelineServiceResponse<S, B, T>
 where
     B: MessageBody,
     S: Service,
     T: Sender + Clone,
 {
+    #[pin]
     fut: S::Future,
     clock: SystemTime,
     inner: Arc<BeelineMiddleware<T>>,
@@ -187,18 +193,22 @@ where
     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
     T: Sender + Clone,
 {
-    type Item = ServiceResponse<StreamLog<B, T>>;
-    type Error = Error;
+    type Output = Result<ServiceResponse<StreamLog<B, T>>, Error>;
 
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let res = futures::try_ready!(self.fut.poll());
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+
+        let res = match futures::ready!(this.fut.poll(cx)) {
+            Ok(res) => res,
+            Err(e) => return Poll::Ready(Err(e)),
+        };
 
         let req = res.request();
-        let inner = self.inner.clone();
+        let inner = this.inner.clone();
         let method = req.method().clone();
         let path = req.path().to_string();
         let headers = req.headers();
-
+        let time = *this.clock;
         let trace = inner.trace.clone();
         let rs = trace.lock().get_root_span();
         {
@@ -219,11 +229,11 @@ where
             }
         }
 
-        Ok(Async::Ready(res.map_body(move |head, body| {
+        Poll::Ready(Ok(res.map_body(move |head, body| {
             ResponseBody::Body(StreamLog {
                 body,
                 size: 0,
-                clock: self.clock,
+                clock: time,
                 inner,
                 status: head.status,
                 path: path.clone(),
@@ -234,7 +244,9 @@ where
 }
 
 #[doc(hidden)]
+#[pin_project(PinnedDrop)]
 pub struct StreamLog<B, T: Sender + Clone> {
+    #[pin]
     body: ResponseBody<B>,
     size: usize,
     clock: SystemTime,
@@ -244,8 +256,9 @@ pub struct StreamLog<B, T: Sender + Clone> {
     method: Method,
 }
 
-impl<B, T: Sender + Clone> Drop for StreamLog<B, T> {
-    fn drop(&mut self) {
+#[pinned_drop]
+impl<B, T: Sender + Clone> PinnedDrop for StreamLog<B, T> {
+    fn drop(self: Pin<&mut Self>) {
         self.inner
             .send(&self.path, &self.method, self.status, self.clock, self.size);
     }
@@ -256,19 +269,21 @@ impl<B: MessageBody, T: Sender + Clone> MessageBody for StreamLog<B, T> {
         self.body.size()
     }
 
-    fn poll_next(&mut self) -> Poll<Option<Bytes>, Error> {
-        match self.body.poll_next()? {
-            Async::Ready(Some(chunk)) => {
-                self.size += chunk.len();
-                Ok(Async::Ready(Some(chunk)))
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes, Error>>> {
+        let this = self.project();
+        match MessageBody::poll_next(this.body, cx) {
+            Poll::Ready(Some(Ok(chunk))) => {
+                *this.size += chunk.len();
+                Poll::Ready(Some(Ok(chunk)))
             }
-            val => Ok(val),
+            val => val,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use actix_web::rt as actix_rt;
     use actix_web::test::{call_service, init_service, read_body, TestRequest};
     use actix_web::{web, App, HttpResponse};
     use beeline::{Client, Config};
@@ -296,39 +311,42 @@ mod tests {
         beeline::test::init(config)
     }
 
-    #[test]
-    fn middleware_basic() {
+    #[actix_rt::test]
+    async fn middleware_basic() {
         let middleware = BeelineMiddleware::new(new_client());
         let mut app = init_service(
             App::new()
                 .wrap(middleware.clone())
                 .service(web::resource("/").to(|| HttpResponse::Ok().json(()))),
-        );
+        )
+        .await;
 
         let res = call_service(
             &mut app,
             TestRequest::with_uri("/")
                 .header("content-type", "text/plain")
                 .to_request(),
-        );
+        )
+        .await;
         assert!(res.status().is_success());
-        assert_eq!(read_body(res), Bytes::from_static(b"null"));
+        assert_eq!(read_body(res).await, Bytes::from_static(b"null"));
         let events = middleware.client.0.write().client.transmission.events();
         // TODO(nlopes): should I expose .fields from Event and also check content?
         assert_eq!(events.len(), 1);
     }
 
-    #[test]
-    fn middleware_basic_failure() {
+    #[actix_rt::test]
+    async fn middleware_basic_failure() {
         let middleware = BeelineMiddleware::new(new_client());
         let mut app = init_service(
             App::new()
                 .wrap(middleware.clone())
                 .service(web::resource("/").to(HttpResponse::Ok)),
-        );
+        )
+        .await;
 
         {
-            let res = call_service(&mut app, TestRequest::with_uri("/missing").to_request());
+            let res = call_service(&mut app, TestRequest::with_uri("/missing").to_request()).await;
             assert_eq!(res.status(), StatusCode::NOT_FOUND);
         }
         let events = middleware.client.0.write().client.transmission.events();
